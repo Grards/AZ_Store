eae3a421fb2c845dc5f86248236a5742c2be5ca6
Steve Grard <stevegrard1@gmail.com>
2023-06-08 16:52:18 +0200

    :hammer: actionProducts fixed. And relink to index.php

--- backend/actionProducts.php
+++ backend/actionProducts.php
@@ -1,54 +1,30 @@
 <?php
-
-echo"<pre>";
-print_r($_POST);
-echo"</pre>";
-
-
 if($_SERVER['REQUEST_METHOD']==='POST'){
-    
+
     $name = $_POST['product_name'];
     $price = $_POST['product_price'];
 
-    //check if the data is ok 
-    // echo $name." ".$price;
-
     //get the json file
-    $json = file_get_contents('shopping-card.json');
+    $json = file_get_contents('../assets/json/shopping-card.json');
 
     //decode the JSON file
     $json_data = json_decode($json,true);
 
-    $newObj = array(
+    $id = sizeof($json_data) + 1;
+    $newObj = [
+        "id" => $id,
         "name" => $name,
         "price" => $price
-    );
+    ];
 
     // Add $newObj to the existing data
     $json_data[] = $newObj;
 
-    // $JsonObject = 
-
     echo json_encode($json_data);
 
     // Save the updated JSON back to the file
-    file_put_contents('shopping-card.json', json_encode($json_data));
+    file_put_contents('../assets/json/shopping-card.json', json_encode($json_data));
 }
 
-
-
-
-
-
-
-
-
-// echo"<pre>";
-// print_r($json);
-// echo"</pre>";
-
-
-// $name = $json_data
-
-
+header('Location: ../index.php');
 ?>
