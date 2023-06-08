c3aba47ead9b227972880ab54105667c47b0e29e
Steve Grard <stevegrard1@gmail.com>
2023-06-08 10:34:59 +0200

    Replace PHP folder by view folder. And add some partials

---
+++ assets/views/partials/_header.php
@@ -0,0 +1,6 @@
+<header id="header">
+    <section id="logo">AZ[store]</section>
+    <nav id="navbar"></nav>
+    <section id="cart"><a href="./assets/views/shopping-cart.php"><img src="./src/img/shopping-cart.svg" alt="shopping cart svg"></a></section>
+    <section id="login"><a href="#">Login</a></section>
+</header>
--- index.php
+++ index.php
@@ -9,10 +9,13 @@
     <title>Az Store</title>
 </head>
 <body>
-    <div id="header">
-        <div id="navbar"></div>
-    </div>
-    <div id="content"></div>
-    <div id="footer"></div>
+    <header id="header">
+        <section id="logo">AZ[store]</section>
+        <nav id="navbar"></nav>
+        <section id="cart"><a href="./assets/views/shopping-cart.php"><img src="./src/img/shopping-cart.svg" alt="shopping cart svg"></a></section>
+        <section id="login"><a href="#">Login</a></section>
+    </header>
+    <main id="content"></main>
+    <footer id="footer"></footer>
 </body>
 </html>
