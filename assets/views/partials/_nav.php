d98b88b6eb30319147d747f9d6338bca62d39ad6
Steve Grard <grard.steve@gmail.com>
2023-06-10 15:34:45 +0200

    :alembic: Work on css with tailwind

--- assets/views/partials/_nav.php
+++ assets/views/partials/_nav.php
@@ -1,8 +1,8 @@
 <nav id="navbar" class="nav basis-8/12 h-12">
-    <ul class = "nav__list flex flex-row text-center h-full justify-evenly w-full">
-        <li class="nav__element h-full w-1/4 bg-red-200"><a class="block h-full w-full" href="/AZ_Store/index.php">Home</a></li>
-        <li class="nav__element h-full w-1/4"><a class="block h-full w-full" href="/AZ_Store/assets/views/checkout.php">Checkout</a></li>
-        <li class="nav__element h-full w-1/4"><a class="block h-full w-full" href="/AZ_Store/index.php">Product</a></li>
-        <li class="nav__element h-full w-1/4"><a class="block h-full w-full" href="/AZ_Store/index.php">Contact</a></li>
+    <ul class = "nav__list flex flex-row h-full w-full justify-center gap-x-5 leading-10">
+        <li class="nav__element h-full bg-red-200 w-28"><a class="block h-full w-full " href="/AZ_Store/index.php">Home</a></li>
+        <li class="nav__element h-full w-28"><a class="block h-full w-full" href="/AZ_Store/assets/views/checkout.php">Checkout</a></li>
+        <li class="nav__element h-full w-28"><a class="block h-full w-full" href="/AZ_Store/index.php">Product</a></li>
+        <li class="nav__element h-full w-28"><a class="block h-full w-full" href="/AZ_Store/index.php">Contact</a></li>
     </ul>
 </nav>
